9a1448d5ac2d4b54bbef978ee26f9f2955d11a0b
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-08-26 22:40:47 +0100

    Using inheritance with Class keyword (extends)

--- OOP/index.js
+++ OOP/index.js
@@ -23,3 +23,39 @@ function createElf(name, weapon) {
 
 const peter = createElf('Peter', 'stones');
 console.log(peter);
+
+// Inheritance
+class Character {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    attack() {
+        return `Attack with ${this.weapon}`
+    }
+}
+
+class Elf extends Character {
+    constructor(name, weapon, type) {
+        super(name, weapon);
+        this.type = type;
+    }
+}
+
+class Ogre extends Character {
+    constructor(name, weapon, color) {
+        super(name, weapon);
+        this.color = color;
+    }
+
+    makeFort() {
+        return 'Strongest fort in the worl made';
+    }
+}
+
+const dolby = new Elf('Dolby', 'cloth', 'house');
+console.log(dolby);
+
+const shrek = new Ogre('Shrek', 'club', 'green');
+console.log(shrek);
