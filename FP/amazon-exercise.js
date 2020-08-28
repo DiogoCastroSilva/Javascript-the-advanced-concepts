fc8f163cb93c060e221a0ac75de27db92be60606
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-08-28 08:46:29 +0100

    Finish Amazon exercise

--- FP/amazon-exercise.js
+++ FP/amazon-exercise.js
@@ -3,7 +3,7 @@ const user = {
     name: 'Kim',
     active: true,
     cart: [],
-    purchase: []
+    purchases: []
 };
 
 // Implement a cart feature:
@@ -16,30 +16,54 @@ const user = {
 // 1. Accept refunds
 // 2. Track user history
 
-function addItemToCart(cart, item) {
-    return [...cart, item];
+console.log(purchaseItem(
+    emptyCart,
+    buyItem,
+    AddTax,
+    addItemToCart
+)(user, { name: 'Laptop', price: 200 }));
+
+const history = [];
+
+const compose = (f, g) => (...args) => f(g(...args));
+
+function purchaseItem(...fns) {
+    history.push(`Purchasing item`);
+    return fns.reduce(compose);
 }
 
-function AddTax(cart, tax = 3) {
-    return cart.map(item => {
-        item.price *= tax / 100;
-        return item;
-    });
+function addItemToCart(user, item) {
+    history.push(`Purchasing item: ${item}, for user: ${user}`);
+    const updatedCart = user.cart.concat(item);
+    return Object.assign({}, user, { cart: updatedCart });
 }
 
-function buyItem(purchase, item) {
-    return [...purchase, item];
+function AddTax(user) {
+    history.push(`Adding tax: ${user}`);
+    const { cart } = user;
+    const tax = 1.3;
+    const updatedCart = cart.map(item => ({
+        name: item.name,
+        price: item.price * tax
+    }));
+    history.push(`After adding tax: ${user}`);
+    return Object.assign({}, user, { cart: updatedCart });
 }
 
-function emptyCart() {
-    return [];
+function buyItem(user) {
+    history.push(`Buying item(s): ${user}`);
+    return Object.assign({}, { purchases: user.purchases.concat(user.cart) });
 }
 
-function acceptRefunds(purchase, cart, item) {
-    const newPurchase = purchase.filter(i => i.name !== item.name);
+function emptyCart(user) {
+    history.push(`Emptying cart: ${user}`);
+    return Object.assign({}, user, { cart: [] });
+}
+
+function acceptRefunds(user, item) {
+    history.push(`Accepting refunds: ${item}, for user: ${user}`);
+    const newPurchases = user.purchases.filter(i => i.name !== item.name);
     const newCart = [...cart, item];
-    return {
-        cart: newCart,
-        purchase: newPurchase
-    };
+    history.push(`Refund accepted: ${user}`);
+    return Object.assign({}, user, { cart: newCart, purchases: newPurchases });
 }
