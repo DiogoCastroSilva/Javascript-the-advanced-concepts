df1ed14327d39d21ed08719dbc80152f14c5ed0d
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-08-30 21:22:09 +0100

    Run a promise in Sequence

--- Async/parallel-sequence-race.js
+++ Async/parallel-sequence-race.js
@@ -11,6 +11,8 @@ const a = () => promisify('a', 100);
 const b = () => promisify('b', 5000);
 const c = () => promisify('c', 3000);
 
+
+
 async function parallel() {
     const promises = [a(), b(), c()];
     const [output1, output2, output3] = await Promise.all(promises);
@@ -30,3 +32,15 @@ async function race() {
 
 // Race --> Return the fastest promise to be completed
 race().then(console.log);
+
+
+
+async function sequence() {
+    const output1 = await a();
+    const output2 = await b();
+    const output3 = await c();
+    return `Sequence is done: ${output1} ${output2} ${output3}`;
+}
+
+// Sequence --> Runs promises in a sequence (first, second, third, ...)
+sequence().then(console.log);
